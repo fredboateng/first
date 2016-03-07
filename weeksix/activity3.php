c07713b0ae65d3e423cad72d320816c612c28d6b
1312897 <fredakanapo@yahoo.co.uk>
2016-03-07 10:17:50 +0000

    my first PHP

--- weeksix/activity3.php
+++ weeksix/activity3.php
@@ -5,10 +5,10 @@
  * Date: 07/03/2016
  * Time: 09:57
  */
-echo "Hello, world\n";
+echo "Hello, world <br>";
 
 // concatenation in php
-echo "Hello," . " " . "world" . "!"."\n";
+echo "Hello," . " " . "world" . "!"."<br>";
 
 // printing sample arithmetic
 
@@ -16,7 +16,7 @@ echo 5*7;
 
 // start using variables
 
-$myname = "Frodo Baggins";
+$myname = "Frodo Baggins"."<br>";
 $myage = 111;
 echo "My name is ".$myname." and I am ".$myage;
 
