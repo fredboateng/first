8043c21c1983012b18c7057cce70b7aac2965c4d
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-07 19:46:25 +0000

    more activities

--- weeksix/activity5.php
+++ weeksix/activity5.php
@@ -7,9 +7,9 @@
  */
 $myArray = array("do","re","mi");
 echo $myArray[0]; // outputs "do"
-echo "<b>";
+echo "<br>";
 $myArray[1]="la"; // modifies position 1 (re)
 echo $myArray[1]; // outputs "la"
-echo "<b>";
+echo "<br>";
 unset($myArray[2]);
 
