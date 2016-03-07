143b80593d9da182ae689cb0b3068095939f344d
1312897 <fredakanapo@yahoo.co.uk>
2016-03-07 10:47:12 +0000

    my first PHP

---
+++ weeksix/activity4.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 07/03/2016
+ * Time: 10:34
+ */
+$numberOfHobbits = 2;
+switch($numberOfHobbits){
+    case 1:
+        echo "1 sad hobbit";
+        break;
+    case 2:
+        echo "2 happy hobbits";
+        break;
+    case 3:
+        echo "3 hobbits are a crowd";
+        break;
+    default:
+        echo "All hobbits have gone home";
+}
