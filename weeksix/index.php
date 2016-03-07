2d485df31aa73ff63f84a522bf4f0fcb3d9629ad
1312897 <fredakanapo@yahoo.co.uk>
2016-03-07 09:46:36 +0000

    my first PHP

---
+++ weeksix/index.php
@@ -0,0 +1,22 @@
+
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 07/03/2016
+ * Time: 09:37
+ */
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+    <p>
+        <?php
+        echo "Hello World";
+        ?>
+    </p>
+    </body>
+
+</html>
