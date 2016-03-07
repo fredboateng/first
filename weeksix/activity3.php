65b9ca2699a81f2cb6804a3c7d0923cb2ee6200e
1312897 <fredakanapo@yahoo.co.uk>
2016-03-07 10:05:21 +0000

    my first PHP

---
+++ weeksix/activity3.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 07/03/2016
+ * Time: 09:57
+ */
+echo "Hello, world";
+
+// concatenation in php
+echo "Hello," . " " . "world" . "!";
+
+// printing sample arithmetic
+
+echo 5*7;
+
+// start using variables
+
+$myname = "Frodo Baggins";
+$myage = 111;
+echo "My name is ".$myname." and I am ".$myage;
+
