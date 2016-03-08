84fc58d9ade29addfa673b677bd69b2f35ea75f7
1312897 <fredakanapo@yahoo.co.uk>
2016-03-08 10:18:25 +0000

    addition of loops

--- weeksix/activity5.php
+++ weeksix/activity5.php
@@ -51,7 +51,7 @@ for($i = 1; $i < 10; $i++){
 
 for($i = 1; $i <= 30; $i++){
     $position = array("st","nd","rd","th");
-    if($i<4 || $i>20){
+    if($i<4 || ($i>20 && $i<24)){
         $a=$i%10;
         $y="";
         switch($a){
