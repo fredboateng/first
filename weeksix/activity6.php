ada312dbd4fc11b07542aa73ba37b772ce26e8d6
1312897 <fredakanapo@yahoo.co.uk>
2016-03-08 10:44:31 +0000

    addition of loops

---
+++ weeksix/activity6.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 08/03/2016
+ * Time: 10:41
+ */
+
+// String Functions
+// get the length of a string and print it to the screen
+$length = strlen("david");
+print $length;
