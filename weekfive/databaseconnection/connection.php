e5eb4b8de278f6dc4cbb39285a2b15aee73d1b4b
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-14 19:40:36 +0000

    Db links

---
+++ weekfive/databaseconnection/connection.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CG
+ * Date: 14/03/2016
+ * Time: 16:43
+ */
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME','bb8e5351eccd82 ');
+define('DB_PASSWORD', 'e5d86e49');
+define('DB_DATABASE', 'kofifred');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
---
+++ weekfive/databaseconnection/login.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CG
+ * Date: 14/03/2016
+ * Time: 18:01
+ */
+include (connection.php);
+if(empty($_POST["username"])||empty($_POST["password"])){
+    echo "Both fields are required.";
+}else{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+}
+$sql = "SELECT uid FROM users WHERE username='$username' and
+        password='$password'";
+
+$result = mysqli_query($dp,$sql);
+
+if(mysqli_num_row($result) == 1){
+    header("location: home.php");
+}else{
+    echo "Incorrect username or password.";
+}
+
