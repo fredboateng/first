db7f2d9a5fd5fb9fe68bff3465a427ec406e61b6
1312897 <fredakanapo@yahoo.co.uk>
2016-03-15 09:43:21 +0000

    more commits

--- weekfive/databaseconnection/login.php
+++ weekfive/databaseconnection/login.php
@@ -8,20 +8,20 @@
 include (connection.php);
 if(empty($_POST["username"])||empty($_POST["password"])){
     echo "Both fields are required.";
-}else{
-    $username=$_POST['username'];
-    $password=$_POST['password'];
-}
-$sql = "SELECT uid FROM users WHERE username='$username' and
+}else {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT uid FROM users WHERE username='$username' and
         password='$password'";
 
-$result = mysqli_query($dp,$sql);
 
-if(mysqli_num_row($result) == 1){
-    //header("location:home.php");
-    echo "hello";
-}else{
-    echo "Incorrect username or password.";
+    $result = mysqli_query($dp, $sql);
+
+    if (mysqli_num_row($result) == 1) {
+        //header("location:home.php");
+        echo "hello";
+    } else {
+        echo "Incorrect username or password.";
+    }
 }
-
 ?>
