e95c3804324a8bf3d0f54ac219aa49caa08e800f
1312897 <fredakanapo@yahoo.co.uk>
2016-03-15 10:11:39 +0000

    more commits

--- weekfive/databaseconnection/connection.php
+++ weekfive/databaseconnection/connection.php
@@ -16,6 +16,6 @@ if (mysqli_connect_errno())
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 else{
-    echo "success connect to db";
+    //echo "success connect to db";
 }
 ?>
