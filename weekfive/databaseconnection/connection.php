b10d90a062c2d7275081d87bfa884706e31377c8
1312897 <fredakanapo@yahoo.co.uk>
2016-03-15 09:52:02 +0000

    more commits

--- weekfive/databaseconnection/connection.php
+++ weekfive/databaseconnection/connection.php
@@ -6,7 +6,7 @@
  * Time: 16:43
  */
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
-define('DB_USERNAME','bb8e5351eccd82 ');
+define('DB_USERNAME','bb8e5351eccd82');
 define('DB_PASSWORD', 'e5d86e49');
 define('DB_DATABASE', 'kofifred');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
