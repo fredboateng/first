8a747ad3b1d34d24d3ee47d346b4e56f3eb4e190
1312897 <fredakanapo@yahoo.co.uk>
2016-03-15 09:40:11 +0000

    more commits

--- weekfive/databaseconnection/login.php
+++ weekfive/databaseconnection/login.php
@@ -18,7 +18,8 @@ $sql = "SELECT uid FROM users WHERE username='$username' and
 $result = mysqli_query($dp,$sql);
 
 if(mysqli_num_row($result) == 1){
-    header("location:home.php");
+    //header("location:home.php");
+    echo "hello";
 }else{
     echo "Incorrect username or password.";
 }
