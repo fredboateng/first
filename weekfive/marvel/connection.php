08efd50e413f5342b4cf6a3ab902561a488983d7
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-19 08:08:37 +0000

    started to code assignment

---
+++ weekfive/marvel/connection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CG
+ * Date: 14/03/2016
+ * Time: 16:43
+ */
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME','bb8e5351eccd82');
+define('DB_PASSWORD', 'e5d86e49');
+define('DB_DATABASE', 'e5d86e49');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if (mysqli_connect_errno())
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else{
+    //echo "success connect to db";
+}
+?>
--- weekfive/marvel/index.php
+++ weekfive/marvel/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Movies</title>
+    <link rel="stylesheet" href="style.css" type ="text/css" >
 </head>
 <body>
 <header>
