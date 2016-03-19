9a564dfa5d37d140418d241cb006c0dbfa9b4fef
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-19 06:37:17 +0000

    started to code assignment

---
+++ weekfive/marvel/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Movies</title>
+</head>
+<body>
+<header>
+    <h1>Welcome to Movies dot Napo</h1>
+</header>
+<main>
+    <h3>
+        Movies are grouped into the following categories
+    </h3>
+    <ul>
+        <li><a href="allmovies.php">Home</a> </li>
+        <li><a href="marvelstudio.php">Display Movies created by Marvel Studios</a> </li>
+        <li><a href="moviesafter2010">Display Movies created after 2010</a> </li>
+        <li><a href="allxmen">Display all X-Men Films</a> </li>
+    </ul>
+</main>
+
+</body>
+</html>
