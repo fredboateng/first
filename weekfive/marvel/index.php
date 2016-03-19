b8a5376087a72e042e6e49a8f1de9fcfa92f8423
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-19 12:16:13 +0000

    started to code assignment

--- weekfive/marvel/index.php
+++ weekfive/marvel/index.php
@@ -14,8 +14,8 @@
         Movies are grouped into the following categories
     </h3>
     <ul>
-        <li><a href="allmovies.php">Home</a> </li>
-        <li><a href="marvelstudio.php">Display Movies created by Marvel Studios</a> </li>
+        <li><a href="allmovies.php">Display all movies</a> </li>
+        <li><a href="marvelmovies.php">Display Movies created by Marvel Studios</a> </li>
         <li><a href="moviesafter2010">Display Movies created after 2010</a> </li>
         <li><a href="allxmen">Display all X-Men Films</a> </li>
     </ul>
