4acaaffc4fc5e83e8fb9560c9edc9880e39d74cf
1312897 <fredakanapo@yahoo.co.uk>
2016-03-21 10:06:10 +0000

    week eight get variables

--- weekeight/helloSelector.php
+++ weekeight/helloSelector.php
@@ -17,6 +17,6 @@
 <p>Where would you like to say hello?</p>
 <p><a href="helloPrinter.php?planet = Earth">Earth</a></p>
 <p><a href="helloPrinter.php?planet = Mars">Mars</a></p>
-<p><a href="helloPrinter.php?planet = Uranus">Uranus/a></p>
+<p><a href="helloPrinter.php?planet = Uranus">Uranus</a></p>
 </body>
 </html>
