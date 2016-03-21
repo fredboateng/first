0daf2b4ac8ff4293f07e75ec178a3c89a472e084
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-21 17:17:40 +0000

    started to code assignment

--- weekeight/inputform.php
+++ weekeight/inputform.php
@@ -16,7 +16,6 @@
 <form action="formResponse.php" method="post">
     <label>Forename</label><input type="text" name="forename"><br>
     <label>Surname</label><input type="text" name="surname"><br>
-
     <!-- <input type="submit" value="Submit"> -->
 </form>
 </body>
