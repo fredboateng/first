114c9e1cba68d8303b8ced10b4673cbf2d455fa6
fredboateng <fredakanapo@yahoo.co.uk>
2016-03-30 20:10:26 +0100

    populating and viewing users

---
+++ databasetrys/submitform.php
@@ -0,0 +1,30 @@
+<?
+
+/**
+ * Created by PhpStorm.
+ * User: CG
+ * Date: 30/03/2016
+ * Time: 19:25
+ */
+/*create connection with database*/
+include ("connection.php");
+
+/*Obtain variables from form*/
+$fullname = $_POST["fullname"];
+$password = $_POST["password"];
+
+/*create the sql query*/
+$sql = "INSERT INTO users(username, password)VALUES ('$fullname','$password')";
+
+// run query
+mysqli_query($db,$sql);
+
+// confirm insert or give an error message
+if(mysqli_query($db,$sql)){
+    echo "input inserted";
+}else{
+    echo "Error ".$sql."<br>".mysqli_error($db);
+}
+
+// redirection
+header("location:viewusers.php");
---
+++ databasetrys/viewusers.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: CG
+ * Date: 30/03/2016
+ * Time: 19:26
+ */
