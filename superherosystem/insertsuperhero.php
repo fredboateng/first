7625cbff98698c35318b5565dba39a45ae4362ee
fredboateng <fredakanapo@yahoo.co.uk>
2016-04-09 13:50:22 +0100

    my superhero system

--- superherosystem/insertsuperhero.php
+++ superherosystem/insertsuperhero.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: CG
- * Date: 09/04/2016
- * Time: 12:18
- */
+include ("connection.php");
+
+// creating variables to the hold superhero details
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$superpower = $_POST['superpower'];
+
+// insert query
+$sql = "INSERT INTO superheros(firstName, lastName, mainSuperPower) VALUES ('$firstname', '$lastname', '$superpower')";
+
+// inserting into the database
+if ($db -> query($sql)){
+}else{
+    echo "Erro: ".$sql."<br>"."Try again";
+}
