13fb528a526d4954cff95e3a9f66dbb9d55c4aaa
fredboateng <fredakanapo@yahoo.co.uk>
2016-04-09 21:56:06 +0100

    my superhero system

--- superherosystem/displayBattle.php
+++ superherosystem/displayBattle.php
@@ -20,8 +20,8 @@
         $sql_query = "SELECT * FROM superherobattles";
     }
 
-    $result = $dp -> query($sql_query);
-    while ($row = $result -> fetch_array())
+    $result = $db -> query($sql_query);
+    while ($row = $result->fetch_array())
     {
         $firstname = $row['firstname'];
         $lastname = $row['lastname'];
