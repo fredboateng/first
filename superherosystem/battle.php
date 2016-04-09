5a7b77a59f4fab3566a6e99a2b5161352c948ab2
fredboateng <fredakanapo@yahoo.co.uk>
2016-04-09 16:54:59 +0100

    my superhero system

--- superherosystem/battle.php
+++ superherosystem/battle.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: CG
- * Date: 09/04/2016
- * Time: 12:19
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Superhero System</title>
+</head>
+<body>
+<header>
+    <h1>The Super-Superhero Systme</h1>
+    <h2>Superhero Home Page</h2>
+    <p><a href="index.php">Return to home ...</a> </p>
+</header>
+<main>
+    <form action="insetBattle.php" method="post">
+        <p>Select the superhero that faught in this battle</p> <select name = "superhero">
+            <?
+            include ("connection.php");
+            $sql_query = "SELECT * FROM superheros";
+            $result = $db->query($sql_query);
+            while($row =$result ->fetch_array())
+            {
+                $firstname = $row['firstName'];
+                $lastname = $row['lastName'];
+                $superheroID = $row['superheroID'];
+                echo"<option value='{$superheroID}'>{$firstname} {$lastname}</option>";
+            }
+            ?>
+        </select><br>
+        <input type="text" name="villan" placeholder="Villian Faught"><br>
+        <input type="submit" text="Record Battle">
+    </form>
+</main>
+
+</body>
+</html>
--- superherosystem/insetBattle.php
+++ superherosystem/insetBattle.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: CG
- * Date: 09/04/2016
- * Time: 12:19
- */
+include ("connection.php");
+$superheroID = $_POST["superhero"];
+$villan = $_POST["villan"];
+
+$sql = "INSERT INTO battle(superhero, villanFought)VALUES ('$superheroID', '$villan')";
+
+if(mysqli_query($db,$sql)) {
+}else{echo "Erro: ".$sql."<br>"."Try again";
+}
+$db -> close();
+header("location:index.php");
