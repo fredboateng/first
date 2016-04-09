eae67d280c4c8c24922ba1e1f5ab9be940c39c70
fredboateng <fredakanapo@yahoo.co.uk>
2016-04-09 17:31:51 +0100

    my superhero system

--- superherosystem/displayBattle.php
+++ superherosystem/displayBattle.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: CG
- * Date: 09/04/2016
- * Time: 12:19
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Superhero System</title>
+</head>
+<body>
+<header>
+    <h1>The Super-Superhero System</h1>
+    <h2>Superhero Home Page</h2>
+</header>
+<main>
+    <?
+    include ("connection.php");
+    if(isset($_GET['id'])){
+        $superheroID = $_GET['id'];
+        $sql_query = "SELECT * FROM superherobattles WHERE superheroID = '$superheroID'";
+    }
+    else {
+        $sql_query = "SELECT * FROM superbattles";
+    }
+
+    $result = $dp -> query($sql_query);
+    while ($row = $result -> fetch_array())
+    {
+        $firstname = $row['firstname'];
+        $lastname = $row['lastname'];
+        $mainSuperpower = $row['mainSuperPower'];
+        $villanFought = $row['villanFought'];
+
+        echo "<article>
+            <p>The superhero known as <strong>{$firstname} {$lastname}</strong> recently fought
+            <strong>{$villanFought} using <strong>{$mainSuperpower}</strong></strong></p>
+        </article>";
+    }
+    ?>
+</main>
+
+</body>
+</html>
+
