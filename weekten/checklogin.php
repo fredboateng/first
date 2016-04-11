9c023da42d130d4f34eb9d4bd8f468121deac9a4
1312897 <fredakanapo@yahoo.co.uk>
2016-04-11 10:20:38 +0100

    week ten

--- weekten/checklogin.php
+++ weekten/checklogin.php
@@ -3,7 +3,7 @@ $username = $_POST["name"];
 $password = $_POST['password'];
 
 if($username == "Fred" && $password =="123"){
-    setcookie('access_level_cookie', 'standarduser');
+    setcookie('access_level_cookie', 'root');
 }
 
 header('location:loggedin.php');
