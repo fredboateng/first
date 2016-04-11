83f4fe5f56bfb00a0e3ff776f2fe715565737f01
fredboateng <fredakanapo@yahoo.co.uk>
2016-04-11 23:37:52 +0100

    cookies and session

--- weekten/checklogin.php
+++ weekten/checklogin.php
@@ -4,7 +4,8 @@ $password = $_POST['password'];
 
 if($username == "Fred" && $password =="123"){
     setcookie('access_level', 'root');
-    setcookie('username', '$_POST["name"]');
+    //setcookie('username', '');
+    $_COOKIE['username']=$username;
 }
 
 header('location:loggedin.php');
