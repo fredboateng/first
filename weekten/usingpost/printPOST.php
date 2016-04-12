20bf63f561b7eed85fcb1a006fa8d0098928d4b4
1312897 <fredakanapo@yahoo.co.uk>
2016-04-12 11:08:09 +0100

    post

---
+++ weekten/usingpost/printPOST.php
@@ -0,0 +1,11 @@
+<?php
+$title = $_POST['title'];
+$content = $_POST['content'];
+$category = $_POST['category'];
+
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 12/04/2016
+ * Time: 10:59
+ */
