37f23985dbdd7867719ebb52b5ad224cb456c350
1312897 <fredakanapo@yahoo.co.uk>
2016-04-12 10:12:34 +0100

    get

---
+++ weekten/usingget/printGET.php
@@ -0,0 +1,11 @@
+<?php
+
+$query = $_GET['query'];
+$type = $_GET['type'];
+echo "<p>I want to know more about {$query} and I am told {$type} is one of its types</p>";
+/**
+ * Created by PhpStorm.
+ * User: 1312897
+ * Date: 12/04/2016
+ * Time: 10:05
+ */
